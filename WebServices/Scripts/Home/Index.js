6b916c321bdaaa8b45762133877bfa057f62200f
vadimZ <01vadim10@gmail.com>
2016-03-28 17:34:46 +0300

    2nd commit

---
+++ WebServices/Scripts/Home/Index.js
@@ -0,0 +1,43 @@
+﻿function selectView(view) {
+    $('.display').not('#' + view + "Display").hide();
+    $('#' + view + "Display").show();
+}
+function getData() {
+    $.ajax({
+        type: "GET",
+        url: "/api/reservation",
+        success: function (data) {
+            $('#tableBody').empty();
+            for (var i = 0; i < data.length; i++) {
+                $('#tableBody').append('<tr><td><input id="id" name="id" type="radio"'
+                + 'value="' + data[i].ReservationId + '" /></td>'
+                + '<td>' + data[i].ClientName + '</td>'
+                + '<td>' + data[i].Location + '</td></tr>');
+            }
+            $('input:radio')[0].checked = "checked";
+            selectView("summary");
+        }
+    });
+}
+$(document).ready(function () {
+    selectView("summary");
+    getData();
+    $("button").click(function (e) {
+        var selectedRadio = $('input:radio:checked')
+        switch (e.target.id) {
+            case "refresh":
+                getData();
+                break;
+            case "delete":
+                break;
+            case "add":
+                selectView("add");
+                break;
+            case "edit":
+                selectView("edit");
+                break;
+            case "submitEdit":
+                break;
+        }
+    });
+});
