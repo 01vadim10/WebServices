f7a8ef4582d81d6a057d1515f5bb4baa5be5be98
vadimZ <01vadim10@gmail.com>
2016-03-28 17:17:22 +0300

    1st commit

---
+++ WebServices/Controllers/ReservationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class ReservationController : ApiController
+    {
+        private IReservationRepository repo = ReservationRepository.getRepository();
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+        [HttpPost]
+        public Reservation CreateReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+        
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
